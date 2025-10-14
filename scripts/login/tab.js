5a48f043d2eae17442df4ca6b0ea40968e4ca03e
jshp3795 <102227625+jshp3795@users.noreply.github.com>
2025-10-14 20:17:07 +0900

    Minor fixes

--- scripts/login/tab.js
+++ scripts/login/tab.js
@@ -1,11 +1,11 @@
 let credentialInfo = null;
 document.getElementById("knuplus_login").addEventListener("click", async (event) => {
     if (!credentialInfo) {
-        alert("KNU+ 확장 프로그램의 기타 탭에서 통합인증 시스템의 아이디와 비밀번호를 먼저 등록해주세요.");
+        alert("KNU+ 확장 프로그램의 계정 탭에서 통합인증 시스템의 아이디와 비밀번호를 먼저 등록해주세요.");
         return;
     }
     if (credentialInfo.type !== "passkey") {
-        alert("KNU+ 확장 프로그램의 기타 탭에서 패스키를 먼저 등록해주세요.");
+        alert("KNU+ 확장 프로그램의 계정 탭에서 패스키를 먼저 등록해주세요.");
         return;
     }
 
